6adcc76f3ca8bf836b39e4ab26bba3c5feeeaf7a
DeepjyotiBhattacharjee <deepjyotibhattacharjee217@gmail.com>
2024-02-16 22:00:22 +0530

    updated setup.py file

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
 from setuptools import find_packages,setup
+from typing import List
 
-def get_requirements():...
+HYPHEN_DOT_E = "-e ."
+
+def get_requirements(file_path) -> List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        if HYPHEN_DOT_E in requirements:
+            requirements.remove(HYPHEN_DOT_E)
+    return requirements
+
+
+
 
 setup(
     name="DiamondPricePrediction",
     version="0.0.1",
     author="Deepjyoti Bhattacharjee",
     author_email="deepjyotibhattacharjee217@gmail.com",
-    # install_requires = get_requirements("requirements.txt"),
+    install_requires = get_requirements("requirements.txt"),
     packages=find_packages()
 )
 
