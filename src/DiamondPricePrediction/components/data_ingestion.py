5c83ef3c8a68c2aa958daf21ec8dbe2e4ff9ccfa
DeepjyotiBhattacharjee <deepjyotibhattacharjee217@gmail.com>
2024-02-20 23:24:14 +0530

    data ingestion completed

--- src/DiamondPricePrediction/components/data_ingestion.py
+++ src/DiamondPricePrediction/components/data_ingestion.py
@@ -43,6 +43,11 @@ class DataIngestion:
 
             logging.info("Data ingestion completed.")
 
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path 
+            )
+
         except Exception as e:
             logging.info("Exception occured at data ingestion stage.")
             raise CustomException(e,sys)
