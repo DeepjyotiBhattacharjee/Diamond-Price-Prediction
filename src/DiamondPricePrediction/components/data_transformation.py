539bf9038e1b22f540dfc8c83c0ad350eaa6f010
DeepjyotiBhattacharjee <deepjyotibhattacharjee217@gmail.com>
2024-02-23 12:59:57 +0530

    updated data transformation code

--- src/DiamondPricePrediction/components/data_transformation.py
+++ src/DiamondPricePrediction/components/data_transformation.py
@@ -84,16 +84,27 @@ class DataTransformation:
             input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
             target_feature_train_df = train_df[target_column_name]
 
-            input_feature_test_df = test_df.drop(columns=[target_column_name,"id"],axis=1 )
+            input_feature_test_df = test_df.drop(columns=[target_column_name,"id"],axis=1)
+            target_feature_test_df = test_df[target_column_name]
 
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
 
             logging.info("Applied preprocessing object on train and test dataframes")
 
+            train_arr = np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
+
             save_object(
                 file_path =  self.data_transformation_config.preprocessor_obj_file_path,
                 obj = preprocessing_obj
+            )
+
+            logging.info("preprocessor pickle file saved")
+
+            return (
+                 train_arr,
+                 test_arr 
             )
         except Exception as e:
             logging.info("Error occured in initiate data transformation ")
