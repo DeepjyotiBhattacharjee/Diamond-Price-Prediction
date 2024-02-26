f95aa10e3fa8fb526da9bfdde47599db9326110c
DeepjyotiBhattacharjee <deepjyotibhattacharjee217@gmail.com>
2024-02-27 01:52:43 +0530

    updated prediction pipeline path

--- src/DiamondPricePrediction/pipelines/prediction_pipeline.py
+++ src/DiamondPricePrediction/pipelines/prediction_pipeline.py
@@ -13,8 +13,8 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            preprocessor_path = os.path.join("/Users/deepjyotibhattacharjee/Developer/Diamond_Price_Prediction/artifacts","preprocessor.pkl")
-            model_path = os.path.join("/Users/deepjyotibhattacharjee/Developer/Diamond_Price_Prediction/artifacts","model.pkl")
+            preprocessor_path = os.path.join("artifacts","preprocessor.pkl")
+            model_path = os.path.join("artifacts","model.pkl")
 
             preprocessor = load_object(preprocessor_path)
             model =  load_object(model_path)
